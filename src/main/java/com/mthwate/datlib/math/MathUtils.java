994bc6b0ea290c15b57a30af4514680c739a20cd
Matthew Benkart <mthwate@mthwate.com>
2015-04-10 18:35:25 -0400

    Enabled the gcd method to work with two "0" values

--- src/main/java/com/mthwate/datlib/math/MathUtils.java
+++ src/main/java/com/mthwate/datlib/math/MathUtils.java
@@ -7,9 +7,6 @@ package com.mthwate.datlib.math;
 public class MathUtils {
 
 	public static int gcd(int a, int b) {
-		if (a == 0 && b == 0) {
-			throw new IllegalArgumentException("Must supply at least 1 nonzero number");
-		}
 		return (b == 0) ? Math.abs(a) : Math.abs(gcd(b, a % b));
 	}
 
--- src/test/java/com/mthwate/datlib/math/MathUtilsTest.java
+++ src/test/java/com/mthwate/datlib/math/MathUtilsTest.java
@@ -10,15 +10,14 @@ public class MathUtilsTest {
 		int range = 10;
 		for (int x = -range; x <= range; x++) {
 			for (int y = -range; y <= range; y++) {
-				try {
-					int gcd = MathUtils.gcd(x, y);
-					Assert.assertEquals(x % gcd, 0);
-					Assert.assertEquals(y % gcd, 0);
-				} catch (IllegalArgumentException e) {
+				int gcd = MathUtils.gcd(x, y);
+				if (gcd == 0) {
 					Assert.assertEquals(x, 0);
 					Assert.assertEquals(y, 0);
+				} else {
+					Assert.assertEquals(x % gcd, 0);
+					Assert.assertEquals(y % gcd, 0);
 				}
-
 			}
 		}
 	}
