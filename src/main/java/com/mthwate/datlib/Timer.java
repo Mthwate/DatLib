0999cc30cfe0e16add64487d4567ed556e7c16ca
Matthew Benkart <mthwate@mthwate.com>
2015-04-25 23:35:36 -0400

    Added the ability to round the seconds retreived from timers

--- src/main/java/com/mthwate/datlib/Timer.java
+++ src/main/java/com/mthwate/datlib/Timer.java
@@ -1,5 +1,7 @@
 package com.mthwate.datlib;
 
+import com.mthwate.datlib.math.MathUtils;
+
 /**
  * @author mthwate
  * @since 1.0
@@ -8,20 +10,53 @@ public class Timer {
 
 	private long start;
 
+	/**
+	 * Creates and starts a new timer.
+	 *
+	 * @since 1.0
+	 */
 	public Timer() {
 		start();
 	}
 
+	/**
+	 * Starts the timer.
+	 *
+	 * @since 1.0
+	 */
 	public void start() {
 		start = System.nanoTime();
 	}
 
+	/**
+	 * Gets the nanoseconds that have passed.
+	 *
+	 * @since 1.0
+	 * @return the nanoseconds that have passed
+	 */
 	public double getNano() {
 		return System.nanoTime() - start;
 	}
 
+	/**
+	 * Gets the seconds that have passed.
+	 *
+	 * @since 1.0
+	 * @return the seconds that have passed
+	 */
 	public double getSec() {
-		return getNano() / 1000000000d;
+		return getNano() / 1_000_000_000;
+	}
+
+	/**
+	 * Gets the seconds that have passed, rounding to the supplied number of decomal places.
+	 *
+	 * @since 1.1
+	 * @param decimals the number of decimal places to round to
+	 * @return the seconds that have passed
+	 */
+	public double getSec(int decimals) {
+		return MathUtils.round(getSec(), decimals);
 	}
 
 }
